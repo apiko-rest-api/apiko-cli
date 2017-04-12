5343111208c1da1e3891ae87020f6cf94ed7d117
mv <mv@mvs-MacBook-Pro.local>
2017-04-12 17:41:50 +0100

    Initial working version.

---
+++ index.js
@@ -0,0 +1,122 @@
+const fs = require('fs')
+const spawn = require('child_process').spawn
+const commandExists = require('command-exists').sync
+const path = require('path')
+
+let cli = {
+  init: {
+    usage: "apiko init <directory_name>\n- Creates a new directory with the specified name and downloads the Apiko starter template to it.\n- You can then navigate to it, run 'npm i' and 'npm run dev' to start Apiko in development mode.",
+    handler () {
+      console.log('Initiating a new Apiko server...')
+      if (fs.existsSync(process.argv[3])) {
+        console.log('The specified directory already exists. Please try a different directory name or remove this one first.')
+        return false
+      }
+
+      if (!commandExists('git')) {
+        console.log('It seems like Git is not installed here. Follow these instructions: https://git-scm.com/book/en/v2/Getting-Started-Installing-Git')
+        return false
+      }
+
+      let cmd = spawn('git', ['clone', 'https://github.com/kasp1/apiko-start.git', process.argv[3]])
+
+      //cmd.stdout.on('data', (data) => {
+      //  console.log(data.toString())
+      //})
+
+      cmd.stderr.on('data', (data) => {
+        console.log(data.toString().replace(/\r|\n|\r/g, ''))
+      })
+
+      return cmd
+    }
+  },
+
+  setup: {
+    usage: "apiko setup <directory_name>\n- Creates a new directory with the specified name and installs and runs Apiko in it.",
+    handler () {
+      let init = cli.init.handler()
+
+      if (init !== false) {
+        init.on('close', (code) => {
+          if (code === 0) {
+            console.log('Setting up a new Apiko server...')
+
+            // change to the created directory
+            process.chdir(process.cwd() + path.sep + process.argv[3])
+
+            // npm install
+            let cmd = spawn('npm', ['install'])
+
+            //cmd.stdout.on('data', (data) => {
+            //  console.log(data.toString())
+            //})
+
+            cmd.stderr.on('data', (data) => {
+              console.log(data.toString().replace(/\r|\n|\r/g, ''))
+            })
+
+            cmd.on('close', (code) => {
+              cli.run.handler('dev')
+            })
+          }
+        })
+      }
+    }
+  },
+
+  run: {
+    usage: "apiko run <config_mode>\n- Starts Apiko with your config mode specified.",
+    handler (env) {
+      if (!env) {
+        if (process.argv[3]) {
+          env = process.argv[3]
+        } else {
+          cli.help.handler('run')
+        }
+      }
+
+      let cmd = spawn('npm', ['run', env])
+
+      cmd.stdout.on('data', (data) => {
+        console.log(data.toString().replace(/\r|\n|\r/, ''))
+      })
+
+      cmd.stderr.on('data', (data) => {
+        console.log(data.toString().replace(/\r|\n|\r/, ''))
+      })
+    }
+  },
+
+  help: {
+    usage: "apiko help <command_name>\n- Displays help for the specified command.",
+    handler (command) {
+      if (!command) {
+        if (cli[process.argv[3]]) {
+          console.log(cli[process.argv[3]].usage)
+        } else {
+          console.log("Apiko CLI - makes it easy to set up a new Apiko server.")
+          console.log('')
+          console.log("apiko setup <directory_name> # to setup a new Apiko server.")
+          console.log('')
+          console.log('Other commands: ', Object.keys(cli).join(', '))
+          console.log("apiko help <command_name> # to read further.")
+          console.log('')
+          console.log('')
+        }
+      } else {
+        console.log(cli[command].usage)
+      }
+    }
+  }
+}
+
+if (process.argv[2]) {
+  if (cli[process.argv[2]]) {
+    cli[process.argv[2]].handler()
+  } else {
+    cli.help.handler()
+  }
+} else {
+  cli.help.handler()
+}
